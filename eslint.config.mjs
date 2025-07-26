848c7692d08ad24c18700c22ab9e35f6ccc93b15
admclamb <adylanmclamb@yahoo.com>
2025-07-25 23:59:20 -0400

    Add eslint and README.md

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,45 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:@typescript-eslint/stylistic-type-checked"
+  ),
+  ...compat.config({
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      project: true,
+    },
+    plugins: ["@typescript-eslint", "drizzle"],
+    rules: {
+      "@typescript-eslint/array-type": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "warn",
+        {
+          prefer: "type-imports",
+          fixStyle: "inline-type-imports",
+        },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/require-await": "off",
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: {
+            attributes: false,
+          },
+        },
+      ],
+    },
+  }),
 ];
 
 export default eslintConfig;
