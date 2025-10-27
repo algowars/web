6451c0f369a067bd945d008d6cce8df3ead72627
admclamb <adylanmclamb@yahoo.com>
2025-10-27 12:57:17 -0400

    Setting up tests and pipeline

---
+++ tests/e2e/stories/01-auth.story.spec.ts
@@ -0,0 +1,10 @@
+import test from "@playwright/test";
+
+test.describe("User Story: Authentication", () => {
+  test("As user I can sign in and see my dashboard", async ({
+    page,
+    request,
+  }) => {});
+
+  test("As a new user I can sign up", async ({ page, request }) => {});
+});
