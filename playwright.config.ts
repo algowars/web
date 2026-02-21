74f2448569cb4153944179ab656c2dbbe0e06fd5
admclamb <adylanmclamb@yahoo.com>
2026-02-21 00:15:32 -0500

    format

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-import dotenv from 'dotenv';
-import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env.playwright') });
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env.playwright") });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests/e2e',
+  testDir: "./tests/e2e",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -22,31 +22,31 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: process.env.APP_BASE_URL ?? 'http://localhost:3000',
+    baseURL: process.env.APP_BASE_URL ?? "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
     },
 
     {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
     },
 
     /* Test against mobile viewports. */
@@ -72,20 +72,21 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'pnpm run dev',
-    url: process.env.APP_BASE_URL ?? 'http://localhost:3000',
+    command: "pnpm run dev",
+    url: process.env.APP_BASE_URL ?? "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
     env: {
-      AUTH0_SECRET: process.env.AUTH0_SECRET ?? '',
-      APP_BASE_URL: process.env.APP_BASE_URL ?? 'http://localhost:3000',
-      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN ?? '',
-      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID ?? '',
-      AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET ?? '',
-      AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE ?? '',
+      AUTH0_SECRET: process.env.AUTH0_SECRET ?? "",
+      APP_BASE_URL: process.env.APP_BASE_URL ?? "http://localhost:3000",
+      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN ?? "",
+      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID ?? "",
+      AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET ?? "",
+      AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE ?? "",
       AUTH0_CALLBACK_URL:
-        process.env.AUTH0_CALLBACK_URL ?? 'http://localhost:3000/account/callback',
+        process.env.AUTH0_CALLBACK_URL ??
+        "http://localhost:3000/account/callback",
       NEXT_PUBLIC_API_SERVER_URL:
-        process.env.NEXT_PUBLIC_API_SERVER_URL ?? 'http://localhost:5035',
+        process.env.NEXT_PUBLIC_API_SERVER_URL ?? "http://localhost:5035",
     },
   },
 });
