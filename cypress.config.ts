d8b6aed37e073579193c058268ade84f17d60add
admclamb <adylanmclamb@yahoo.com>
2026-03-30 23:31:04 -0400

    not allow cypress env

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 import "dotenv/config";
 
 export default defineConfig({
-  allowCypressEnv: true,
+  allowCypressEnv: false,
 
   e2e: {
     baseUrl: process.env.CYPRESS_BASE_URL ?? "http://localhost:3000",
