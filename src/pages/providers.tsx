cb03895c7cf6469720157e83269d91e190ef85a5
admclamb <adylanmclamb@yahoo.com>
2026-06-02 20:19:36 -0400

    Update providers

---
+++ src/pages/providers.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import * as React from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { Toaster } from "sonner";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { MainErrorFallback } from "@/components/errors/main-error-fallback";
+import { queryConfig } from "@/shared/lib/react-query";
+
+type AppProviders = {
+  children: React.ReactNode;
+};
+
+export default function AppProviders({ children }: AppProviders) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: queryConfig,
+      })
+  );
+
+  return (
+    <ErrorBoundary FallbackComponent={MainErrorFallback}>
+      <QueryClientProvider client={queryClient}>
+        <AccountInitializer />
+        <Toaster position="top-right" />
+        {children}
+        {process.env.NODE_ENV === "development" && <ReactQueryDevtools />}
+      </QueryClientProvider>
+    </ErrorBoundary>
+  );
+}
