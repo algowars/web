6509c8b4ffcdf3e90b26c000613ed6a6b1914048
admclamb <adylanmclamb@yahoo.com>
2026-06-06 21:20:42 -0400

    fix panels

--- src/pages/problems/problem/problem-layout.tsx
+++ src/pages/problems/problem/problem-layout.tsx
@@ -9,33 +9,76 @@ export default function ProblemLayout() {
     direction: "horizontal",
     children: [
       {
-        name: "Code",
         key: "code",
-        defaultSize: 55,
+        name: "Code",
+        defaultSize: 50,
         icon: (
           <CodeXml size={16} className="text-green-600 dark:text-green-400" />
         ),
-        component: (
-          <>
-            <SolutionEditor />
-          </>
-        ),
+        component: <SolutionEditor placeholder="Main solution code" />,
       },
       {
+        key: "right-column",
+        defaultSize: 50,
         direction: "vertical",
-        defaultSize: 45,
         children: [
           {
-            component: <p>Hello world</p>,
-            key: "description",
-            name: "Description",
-            defaultSize: 70,
-            icon: (
-              <FileText
-                size={16}
-                className="text-blue-600 dark:text-blue-400"
-              />
-            ),
+            key: "problem",
+            name: "Problem",
+            defaultSize: 55,
+            children: [
+              {
+                key: "description",
+                name: "Description",
+                icon: (
+                  <FileText
+                    size={16}
+                    className="text-blue-600 dark:text-blue-400"
+                  />
+                ),
+                component: <p>Hello world</p>,
+              },
+              {
+                key: "examples",
+                name: "Examples",
+                icon: (
+                  <FileText
+                    size={16}
+                    className="text-sky-600 dark:text-sky-400"
+                  />
+                ),
+                component: <p>Example inputs and outputs go here.</p>,
+              },
+            ],
+          },
+          {
+            key: "execution",
+            name: "Execution",
+            defaultSize: 45,
+            children: [
+              {
+                key: "console",
+                name: "Console",
+                icon: (
+                  <FileText
+                    size={16}
+                    className="text-purple-600 dark:text-purple-400"
+                  />
+                ),
+                component: <p>Run output will appear here.</p>,
+              },
+              {
+                key: "tests",
+                name: "Tests",
+                icon: (
+                  <FileText
+                    size={16}
+                    className="text-indigo-600 dark:text-indigo-400"
+                  />
+                ),
+                component: <p>Test results will appear here.</p>,
+              },
+            ],
           },
         ],
       },
--- src/shared/components/ui/resizable.tsx
+++ src/shared/components/ui/resizable.tsx
@@ -1,13 +1,21 @@
-"use client"
+"use client";
 
-import * as ResizablePrimitive from "react-resizable-panels"
+import * as ResizablePrimitive from "react-resizable-panels";
 
-import { cn } from "@/shared/lib/utils"
+import { cn } from "@/shared/lib/utils";
+
+type ResizablePanelGroupProps = Omit<
+  ResizablePrimitive.GroupProps,
+  "direction"
+> & {
+  direction?: "horizontal" | "vertical";
+};
 
 function ResizablePanelGroup({
   className,
+  direction,
   ...props
-}: ResizablePrimitive.GroupProps) {
+}: ResizablePanelGroupProps) {
   return (
     <ResizablePrimitive.Group
       data-slot="resizable-panel-group"
@@ -15,13 +23,14 @@ function ResizablePanelGroup({
         "flex h-full w-full aria-[orientation=vertical]:flex-col",
         className
       )}
+      orientation={direction}
       {...props}
     />
-  )
+  );
 }
 
 function ResizablePanel({ ...props }: ResizablePrimitive.PanelProps) {
-  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />
+  return <ResizablePrimitive.Panel data-slot="resizable-panel" {...props} />;
 }
 
 function ResizableHandle({
@@ -29,7 +38,7 @@ function ResizableHandle({
   className,
   ...props
 }: ResizablePrimitive.SeparatorProps & {
-  withHandle?: boolean
+  withHandle?: boolean;
 }) {
   return (
     <ResizablePrimitive.Separator
@@ -44,7 +53,7 @@ function ResizableHandle({
         <div className="z-10 flex h-6 w-1 shrink-0 rounded-lg bg-border" />
       )}
     </ResizablePrimitive.Separator>
-  )
+  );
 }
 
-export { ResizableHandle, ResizablePanel, ResizablePanelGroup }
+export { ResizableHandle, ResizablePanel, ResizablePanelGroup };
