b81a7ff8b0beaa079ee1cd4f79229d9382a6e183
admclamb <adylanmclamb@yahoo.com>
2026-06-07 14:13:42 -0400

    update some things

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,5 @@
+import DashboardLayout from "@/pages/dashboard/dashboard-layout";
+
+export default function DashboardPage() {
+  return <DashboardLayout />;
+}
--- src/app/problems/[slug]/page.tsx
+++ src/app/problems/[slug]/page.tsx
@@ -1,5 +1,25 @@
 import ProblemLayout from "@/pages/problems/problem/problem-layout";
 
+export const generateMetadata = async ({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) => {
+  try {
+    const slug = (await params).slug;
+    const problem = await getProblemBySlug({ slug });
+    return { title: problem.title, description: problem.question };
+  } catch {
+    return { title: "Problem" };
+  }
+};
+
+export const preloadData = async (slug: string) => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(getProblemBySlugQueryOptions(slug));
+}
+
 export default function ProblemPage() {
   return <ProblemLayout />;
 }
