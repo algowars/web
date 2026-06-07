aaa62f1081216e07de6974a21ddff13cb3578676
admclamb <adylanmclamb@yahoo.com>
2026-06-07 08:37:50 -0400

    add language support

--- src/domains/workspace/solution-editor/components/solution-editor.tsx
+++ src/domains/workspace/solution-editor/components/solution-editor.tsx
@@ -1,21 +1,52 @@
 "use client";
 
+import type { Extension } from "@uiw/react-codemirror";
 import CodeMirror, { ReactCodeMirrorProps } from "@uiw/react-codemirror";
 import { vscodeDark, vscodeLight } from "@uiw/codemirror-theme-vscode";
-
 import { cn } from "@/shared/lib/utils";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+import {
+  getLanguageExtensionsById,
+  type LanguageExtensionOptions,
+} from "../extensions/language-extension-map";
 
 interface SolutionEditorProps extends ReactCodeMirrorProps {
   languageId?: number;
+  languageOptions?: LanguageExtensionOptions;
 }
 
-export default function SolutionEditor(props: SolutionEditorProps) {
+export default function SolutionEditor({
+  languageId = 0,
+  languageOptions,
+  ...props
+}: SolutionEditorProps) {
   const { resolvedTheme } = useTheme();
+  const [languageExtensions, setLanguageExtensions] = useState<Extension[]>([]);
+
+  useEffect(() => {
+    let cancelled = false;
+
+    (async () => {
+      const extensions = await getLanguageExtensionsById(
+        languageId,
+        languageOptions
+      );
+      if (!cancelled) {
+        setLanguageExtensions(extensions);
+      }
+    })();
+
+    return () => {
+      cancelled = true;
+    };
+  }, [languageId, languageOptions]);
 
   return (
     <CodeMirror
       {...props}
+      extensions={[...languageExtensions, ...(props.extensions ?? [])]}
       height={props.height ?? "100%"}
       className={cn("h-full", props.className)}
       theme={resolvedTheme === "light" ? vscodeLight : vscodeDark}
