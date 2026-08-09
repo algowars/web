6538f3b4110eafacddadafa4e133d900e3d36d79
admclamb <adylanmclamb@yahoo.com>
2026-08-08 22:49:45 -0400

    Call the problem layout

--- src/views/problems/problems-layout.tsx
+++ src/views/problems/problems-layout.tsx
@@ -1,51 +1,17 @@
 "use client";
 
+import { useLoadProblems } from "@/domains/problem/hooks/use-load-problems";
 import ProblemTable from "@/domains/problem/tables/problem-table";
-import { ProblemEvents } from "@/domains/problem/state/problem-events";
-import {
-  selectProblemsError,
-  selectProblemsPage,
-  selectProblemsSize,
-  selectProblemsTimestamp,
-} from "@/domains/problem/state/problem-slice";
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle,
 } from "@/shared/components/ui/card";
-import { useAppDispatch, useAppSelector } from "@/shared/state/hooks";
 import SidebarLayout from "@/shared/layouts/sidebar-layout/sidebar-layout";
-import { useEffect } from "react";
-import { toast } from "sonner";
 
 export default function ProblemsLayout() {
-  const dispatch = useAppDispatch();
-  const page = useAppSelector(selectProblemsPage);
-  const size = useAppSelector(selectProblemsSize);
-  const timestamp = useAppSelector(selectProblemsTimestamp);
-  const error = useAppSelector(selectProblemsError);
-
-  useEffect(() => {
-    if (typeof timestamp !== "string" || timestamp.trim() === "") {
-      return;
-    }
-
-    dispatch(
-      ProblemEvents.loadProblemsRequested({
-        page,
-        size,
-        timestamp,
-      })
-    );
-  }, [dispatch, page, size, timestamp]);
-
-  useEffect(() => {
-    if (error) {
-      toast.error("Error loading problems", { description: error });
-    }
-  }, [error]);
-
+  useLoadProblems();
   return (
     <SidebarLayout breadcrumbs={[]}>
       <div className="px-2 md:px-4 pb-2 md:pb-4 grid grid-cols-12 gap-4">
