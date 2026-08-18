981366bd724ef51064076633e3b52bf5ef2f75ee
admclamb <adylanmclamb@yahoo.com>
2026-08-17 22:06:22 -0400

    update api

--- src/domains/user/api/get-account.ts
+++ src/domains/user/api/get-account.ts
@@ -1,1 +1,29 @@
-export const getAccount = () => {};
+import { queryOptions, useQuery } from "@tanstack/react-query";
+
+export const getAccount = ({ accessToken, abortController }) => {
+  // api request
+};
+
+export const getAccountQueryOptions = ({ accessToken, abortController }) => {
+  return queryOptions({
+    queryKey: ["account", accessToken],
+    queryFn: () => getAccount({ accessToken, abortController }),
+  });
+};
+
+type GetAccountParams = {
+  accessToken: string;
+  abortController: AbortController;
+  queryConfig?: QueryConfig<typeof getAccount>;
+};
+
+export const useAccount = ({
+  accessToken,
+  abortController,
+  queryConfig,
+}: GetAccountParams) => {
+  return useQuery({
+    ...getAccountQueryOptions({ accessToken, abortController }),
+    ...queryConfig?.(),
+  });
+};
